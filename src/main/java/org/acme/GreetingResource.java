5ccde197da8fc6038675a33c90513947d05b764c
Sébastien Vassal <sebastien.v@evialog.com>
2021-01-26 18:17:13 +0100

    [WebService 2]
    Signed-off-by: Sébastien Vassal <sebastien.v@evialog.com>

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,21 +1,20 @@
 package org.acme;
 
-import com.dataaccess.webservicesserver.TextCasingSoapType;
+import net.gcomputer.webservices.DilbertSoap;
+import org.jboss.logging.Logger;
 
-import javax.enterprise.inject.spi.CDI;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import org.jboss.logging.Logger;
 
 @Path("/hello-resteasy")
 public class GreetingResource {
     private static final Logger LOGGER = Logger.getLogger(GreetingResource.class.getName());
 
-    TextCasingSoapType ws = CDI.current().select(TextCasingSoapType.class).get();
+    @Inject
+    private DilbertSoap ws;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -23,7 +22,7 @@ public class GreetingResource {
         String result = null;
 
         try {
-            result = ws.invertStringCase("tEST STRING.");
+            result = ws.todaysDilbert();
         } catch (Exception e) {
             LOGGER.warn(e.getMessage(), e);
             result = "Failure.";
